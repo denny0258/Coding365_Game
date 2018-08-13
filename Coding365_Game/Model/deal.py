53368cefa893309b1c84d925599d29ae69456df1
Hans Chiu <chiuhans111@gmail.com>
2018-08-13 11:46:04 +0800

    test program (wip)

--- Coding365_Game/Model/deal.py
+++ Coding365_Game/Model/deal.py
@@ -4,8 +4,21 @@
 #python c 對接 邱柏翰
 
 from ..Adapter import cAdapter
+import os
 
 def Take_card():
     pass
 def Test(Test_Data):
-    pass
+    s = ''
+    for i in Test_Data:
+        if Test_Data[i]:
+            s+=i
+    adapter = cAdapter.GetAdapter(os.path.abspath("./deal.exe"))
+    adapter.start()
+    
+    result = adapter.call(s)
+    
+    adapter.stop()
+    d = {}
+    d[result] = True
+    return d
