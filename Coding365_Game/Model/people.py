9e57e8697971bb86ba00907317c2341747671954
ethan <ethan67988@gmail.com>
2018-08-13 12:00:51 +0800

    remove error

--- Coding365_Game/Model/people.py
+++ Coding365_Game/Model/people.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 #安安
 def Test(Test_Data):
-    if Test_Data.get("Hello",None) == True:
+    if Test_Data.get("Hello",None) == True :
         return {"World":True}
-
- 
-#def Get_people():
  #   pass
 #def Test(Test_Data):
  #   pass
