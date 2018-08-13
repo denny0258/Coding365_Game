395a16e032b957e5df25a1c03b40955132c19386
Cheshirefish <qwert211406@gmail.com>
2018-08-13 14:24:59 +0800

    還沒改完

--- Coding365_Game/Model/monitor.py
+++ Coding365_Game/Model/monitor.py
@@ -4,6 +4,6 @@
 #    pass
 #def Test(Test_Data):
 #    pass
-def Test(Test_Data):
-    if Test_Data.get("Hello", None) == True:
-        return {"World" : True}
+#def Test(Test_Data):
+#    if Test_Data.get("Hello", None) == True:
+#        return {"World" : True}
