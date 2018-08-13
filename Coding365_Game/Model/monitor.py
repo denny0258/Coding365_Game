a219cb74b9f1c177c753890b44000cff3af25176
Cheshirefish <qwert211406@gmail.com>
2018-08-13 11:27:02 +0800

    NEW

--- Coding365_Game/Model/monitor.py
+++ Coding365_Game/Model/monitor.py
@@ -1,17 +1,6 @@
 # -*- coding: utf-8 -*-
 #如果爆炸就跳出
-def Test(Test):
-    if Test.get("Hello", None) == True:
-        return {"World",True}
-
-'''
 def Check_21():
     pass
-<<<<<<< HEAD
-def Test(Test):
-    pass
-'''
-=======
 def Test(Test_Data):
     pass
->>>>>>> SORA/Develop
