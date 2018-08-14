6b20f6f62052a999381d12a4d71a59011f7b2d1f
SORA <window930030@gmail.com>
2018-08-14 15:55:40 +0800

    Check_21 OK

--- Coding365_Game/Model/monitor.py
+++ Coding365_Game/Model/monitor.py
@@ -1,30 +1,51 @@
 # -*- coding: utf-8 -*-
-#如果爆炸就跳出
+# 如果爆炸就跳出
+
 
 def Check_21(income):
-    bool1=""
-    dict1={}
+    bool1 = ""
+    dict1 = {}
     for j in income.keys():
-        dict1[j]=bool1
-        if income[j]["life"]=='False':
-            dict1[j]="False"
+        dict1[j] = bool1
+        if income[j]["life"] == 'False':
+            dict1[j] = "False"
         else:
-            point=0
+            point = 0
             for k in range(len(income[j]["Card"])):
-                if (income[j]["Card"])[k][1]>10:
-                    point+=10
+                if (income[j]["Card"])[k][1] > 10:
+                    point += 10
                 else:
-                    point+=(income[j]["Card"])[k][1]
-                if point>21:
-                    dict1[j]="False"
+                    point += (income[j]["Card"])[k][1]
+                if point > 21:
+                    dict1[j] = "False"
                 else:
-                    dict1[j]="True"
+                    dict1[j] = "True"
     return dict1
-# e={"Player_1" : {"life":'False',"Card":[('s',2)]},"Player_2" : {"life":'True',"Card":[('s',3),('h',5),('d',6),('c',9)]},"Player_3" : {"life":'True',"Card":[('d',3),('h',4),('s',6),('c',7)]}}
-# print(Check_21(e))
-# f={"Player_1" : {"life":'False',"Card":[('s',7)]},"Player_2" : {"life":'True',"Card":[('s',4),('h',6),('d',7),('c',8)]},"Player_3" : {"life":'True',"Card":[('d',2),('s',3),('s',6),('c',6)]},"Player_4" : {"life":'True',"Card":[('d',1),('d',3),('s',5),('c',13)]}}
-# print(Check_21(f))
+
 
 def Test(Test_Data):
-   if Test_Data.get("Hello", None) == True:
-       return {"World" : True}
+    if Test_Data.get("Hello", None) == True:
+        return {"World": True}
+
+
+def Test_inside():
+    # ------------------------------------------------------------
+    Data_1 = {"Player_1": {"life": 'False', "Card": [('s', 2)]},
+              "Player_2": {"life": 'True', "Card": [('s', 3), ('h', 5), ('d', 6), ('c', 9)]},
+              "Player_3": {"life": 'True', "Card": [('d', 3), ('h', 4), ('s', 6), ('c', 7)]},
+              "Player_4": {"life": 'True', "Card": [('d', 7), ('s', 3), ('s', 7), ('c', 3)]},
+              "Player_5": {"life": 'True', "Card": [('d', 13), ('s', 6), ('s', 5), ('c', 8)]}
+              }
+    # ------------------------------------------------------------
+    Data_2 = {"Player_1": {"life": 'False', "Card": [('s', 7)]},
+              "Player_2": {"life": 'True', "Card": [('s', 4), ('h', 6), ('d', 7), ('c', 8)]},
+              "Player_3": {"life": 'True', "Card": [('d', 2), ('s', 3), ('s', 6), ('c', 6)]}
+              }
+
+    print(Check_21(Data_1))
+
+    print(Check_21(Data_2))
+
+
+if __name__ == "__main__":
+    Test_inside()
