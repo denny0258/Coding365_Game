f19c16b37e8c686cc9f8ddfe8177856792d6f2be
kloan99622 <40928146+kloan99622@users.noreply.github.com>
2018-08-14 15:11:06 +0800

    new money

--- Coding365_Game/Model/money.py
+++ Coding365_Game/Model/money.py
@@ -1,7 +1,39 @@
 # -*- coding: utf-8 -*-
-#金錢系統
+# 金錢系統``
+
+from operator import add
+from operator import sub
+from functools import reduce
+
+
+def Get_Chip():  # 模擬下注 function
+    return{
+        'player_1': {'Chip': 1, 'InitChip': 10},
+        'player_2': {'Chip': 2, 'InitChip': 10},
+        'player_3': {'Chip': 3, 'InitChip': 10},
+    }
+
+
+def settle_accounts():  # 模擬勝負結算 function
+    return{
+        'Win': 'player_1'
+    }
+
+
 def Get_money():
-    pass
+    Chip_Remain = {}
+    [Chip_Remain.update( {key: value['InitChip'] - value['Chip']})for key, value in Get_Chip().items()]
+    [Chip_Remain.update( {key: value + reduce(add, [chip for chip in Chip_Remain.values()])})for key, value in Chip_Remain.items()if settle_accounts()['Win'] in key]
+
+    return(Chip_Remain)
+
+print(Get_money())
+
+
+if __name__ == '__main__':
+    Get_money()
+
+
 def Test(Test_Data):
     if Test_Data.get("Hello", None) == True:
-        return {"World":True} 
+        return {"World": True}
