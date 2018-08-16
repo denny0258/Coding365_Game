e200115c215cfe51b52749e73f1676650fbfc644
ethan <ethan67988@gmail.com>
2018-08-16 12:26:20 +0800

    e

--- Coding365_Game/Model/people.py
+++ Coding365_Game/Model/people.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
-#安安
-def Test(Test_Data):
-    if Test_Data.get("Hello",None) == True :
-        return {"World":True}
- #   pass
-#def Test(Test_Data):
- #   pass
+# def Player_counts() :
+#     players = int(input())
+#     return players
+
+def Get_people():
+    total = int(input())    
+    while total > 10 :
+        total=int(input())
+    players = int(input())
+    computer= total-players
+    dict1={}
+    dict1["Player_counts"]=players
+    dict2={}
+    dict2["computer_counts"]=computer
+    return dict1
+
+
+
+#def computer_counts(Get_people) :
+    # computer= total-players
+    # return computer
+# def Test(Test_Data):
+#    if Test_Data.get("Hello", None) == True:
+#        return {"World" : True}
+
+print(Get_people())
+
+
