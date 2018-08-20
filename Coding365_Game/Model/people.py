fbdbba4b0159b947259359f59282f3a3ff741ad7
ethan <ethan67988@gmail.com>
2018-08-20 13:47:28 +0800

    people finish
    good

--- Coding365_Game/Model/people.py
+++ Coding365_Game/Model/people.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 # def Player_counts() :
-#     players = int(input())
-#     return players
-
-def Get_people():
-    total = int(input())    
-    while total > 10 :
-        total=int(input())
-    players = int(input())
-    computer= total-players
+def check_people():
     dict1={}
-    dict1["Player_counts"]=players
-    dict2={}
-    dict2["computer_counts"]=computer
+    print("Welcome ,please enter players number")
+    P=int(input())
+    print("Please enter computer players number")
+    AI=int(input())
+    all_player = P + AI 
+    while all_player > 10:
+        print("total player must less than 10,please enter again ")
+        print("please enter players number")
+        P=int(input())
+        print("Please enter computer players number")
+        AI=int(input())
+        all_player = P + AI
+        if all_player <10:
+            break
+    dict1["player"]=P
+    dict1["computer"]=AI
     return dict1
+#print(check_people())
 
+def Test(Test_Data):
+    if Test_Data.get("Hello", None) == True:
+    return {"World" : True}
 
-
-#def computer_counts(Get_people) :
-    # computer= total-players
-    # return computer
-# def Test(Test_Data):
-#    if Test_Data.get("Hello", None) == True:
-#        return {"World" : True}
-
-print(Get_people())
 
 
