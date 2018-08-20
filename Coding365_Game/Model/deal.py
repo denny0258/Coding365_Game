4fe97e2bd64c2284c7920f65d642634627e9657d
SORA <window930030@gmail.com>
2018-08-20 14:36:13 +0800

    New

--- Coding365_Game/Model/deal.py
+++ Coding365_Game/Model/deal.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
-#發牌 陳敦捷
-#python c 對接 邱柏翰
+# 發牌 陳敦捷
+# python c 對接 邱柏翰
 
-from Model.c import shuffle
+try:
+    from Model.c import shuffle
+except:
+    from c import shuffle
+
 
 def Take_card(inputs):
     card = shuffle.Take_Card(inputs["RE"])
@@ -20,9 +24,9 @@ def Take_card(inputs):
 def Test():
     print('deal.py 發牌測試')
 
-    for times in range(3): # 總共進行三次測試
-        re = True # 是否需要洗牌
-        cards = [] # 儲存所有牌面
+    for times in range(3):  # 總共進行三次測試
+        re = True  # 是否需要洗牌
+        cards = []  # 儲存所有牌面
 
         try:
             for i in range(52):
@@ -33,7 +37,7 @@ def Test():
             print('發牌過程發生例外導致測試失敗，目前牌面陣列：')
             print(cards)
             return False
-        
+
         # 計算各種花色數量
         counts = {
             "s": [0]*14,
@@ -49,44 +53,41 @@ def Test():
             if suit not in counts:
                 print('牌面花色錯誤導致測試失敗:', suit)
                 return False
-            
-            if number<0 or number>13:
+
+            if number < 0 or number > 13:
                 print('牌面數字範圍錯誤導致測試失敗：', number)
                 return False
 
-            counts[suit][number]+=1
-        
+            counts[suit][number] += 1
+
         count_pass = True
         for suit in counts:
             count = counts[suit]
-            for i in range(1,13+1):
+            for i in range(1, 13+1):
                 n = count[i]
-                if n!=1:
-                    print('牌數量有誤：',suit, i, '數量:', n)
+                if n != 1:
+                    print('牌數量有誤：', suit, i, '數量:', n)
                     count_pass = False
-        
+
         if not count_pass:
             print('牌面有誤，測試失敗')
             return False
-        
-        for i in range(5): # 不洗牌嘗試取得5張牌
+
+        for i in range(5):  # 不洗牌嘗試取得5張牌
             try:
-                no_card = Take_card({"RE":False})["Card"] 
+                no_card = Take_card({"RE": False})["Card"]
                 if no_card is not None:
                     print('發完牌並沒有回傳 None ，測試失敗')
                     return False
             except:
                 print('嘗試在發完牌後要牌發生例外，測試失敗')
                 return False
-        
+
         print("通過測試", times+1, "以下列出52張牌")
         print(cards)
-    
+
     return True
-    
-    
-
-        
-        
 
 
+if __name__ == "__main__":
+    Test()
