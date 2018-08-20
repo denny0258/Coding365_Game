fe19bb5f5dab12e76a754bea497eda618bb6e763
SORA <window930030@gmail.com>
2018-08-20 19:25:06 +0800

    money OK

--- Coding365_Game/Model/money.py
+++ Coding365_Game/Model/money.py
@@ -1,38 +1,32 @@
 # -*- coding: utf-8 -*-
 # 金錢系統
 
-peoples = ["Player_1","Player_2","Player_3","Player_4"]  #最一開始的人
-delta_Money = {"Player_3":75}   #贏得總量
-withdraw = {"Player_1": -60, "Player_2": -25, "Player_3": -15, "Player_4": -55}
-
 people_Account = {}
+
 
 def Get_money(people):
     global people_Account
-    [people_Account.update({key: people_Account.get(key, 50)}) for key in people]
-    return(people_Account)
+    [people_Account.update({key: people_Account.get(key, 50)})
+     for key in people]
+    return({i: people_Account.get(i, 50) for i in people})
+
 
 def Add_money(delta_Money_1):
     global people_Account
-    [people_Account.update({key: people_Account.get(key, 0) + delta_Money_1.get(key, 0)}) for key, value in people_Account.items()]
-    
+    [people_Account.update({key: people_Account.get(key, 0) + value})
+     for key, value in delta_Money_1.items()]
+
+
 def Take_money(withdraw_1):
     global people_Account
     A = {}
-    for k,v in withdraw_1.items():
-        if people_Account.get(k,50) + v < 0:
-            A.update({k:False})
+    for k, v in withdraw_1.items():
+        if people_Account.get(k, 50) + v < 0:
+            A.update({k: False})
         else:
-            A.update({k:True})
+            A.update({k: True})
 
     return A
-
-    
-if __name__ == "__main__":
-    print(Get_money(peoples))
-    print(Add_money(delta_Money))
-    print(Take_money(withdraw))
-
 
 
 def Test(Test_Data):
