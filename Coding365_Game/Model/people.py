8b5d6a91535463f49c4a1b8a0f3bc7d78be9d6c6
ethan <ethan67988@gmail.com>
2018-08-20 15:26:13 +0800

    good

--- Coding365_Game/Model/people.py
+++ Coding365_Game/Model/people.py
@@ -6,6 +6,8 @@ def check_people():
     P=int(input())
     print("Please enter computer players number")
     AI=int(input())
+    print("Please enter computer players difficuilty (1~10)")
+    dif=int(input())
     all_player = P + AI 
     while all_player > 10:
         print("total player must less than 10,please enter again ")
@@ -13,17 +15,20 @@ def check_people():
         P=int(input())
         print("Please enter computer players number")
         AI=int(input())
+        print("Please enter computer players difficuilty (1~10)")
+        dif=int(input())
         all_player = P + AI
         if all_player <10:
             break
     dict1["player"]=P
     dict1["computer"]=AI
+    dict1["difficuilty"]=dif
     return dict1
-#print(check_people())
+print(check_people())
 
-def Test(Test_Data):
-    if Test_Data.get("Hello", None) == True:
-    return {"World" : True}
+#def Test(Test_Data):#
+    #if Test_Data.get("Hello", None) == True:
+    #return {"World" : True}
 
 
 
